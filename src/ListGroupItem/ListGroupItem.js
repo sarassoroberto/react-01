75ec7a29bf40bf725979abd8d84647faf86ef0fd
sarassoroberto <sarassoroberto@gmail.com>
2020-04-07 08:21:36 +0200

    aggiunta icona fa

--- src/ListGroupItem/ListGroupItem.js
+++ src/ListGroupItem/ListGroupItem.js
@@ -2,13 +2,22 @@ import React from 'react' //preso da node module
 
 const ListGroupItem = (props) => {
 
+    console.log(props.content === undefined)
+
     return(
         <a href="#" className="list-group-item list-group-item-action">
             <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">{props.heading}</h5>
+                <h5 className="mb-1">
+                    {
+                        props.content ?
+                        <i class="fa fa-plus-circle pr-1"></i>
+                        : null
+                    }
+                    {props.heading}
+                </h5>
                 <small>{props.right}</small>
             </div>
-            <p className="mb-1"><small>{props.content}</small></p>
+            <p className="mb-1 d-none"><small>{props.content}</small></p>
         </a>
     )
 }
