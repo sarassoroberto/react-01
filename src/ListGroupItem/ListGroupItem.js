fd75cf9a42be5cbef93c1b94f238de93997715d8
sarassoroberto <sarassoroberto@gmail.com>
2020-04-09 11:12:37 +0200

    conditional rendering ok

--- src/ListGroupItem/ListGroupItem.js
+++ src/ListGroupItem/ListGroupItem.js
@@ -7,14 +7,18 @@ const ListGroupItem = (props) => {
         <div className="list-group-item list-group-item-action">
             <div className="d-flex w-100 justify-content-between">
                 <h5 className="mb-1">
-                    <i className="fa fa-plus-circle pr-1"></i> 
+                    {props.content ? <i className="fa fa-plus-circle pr-1"></i> : null  }
                     {props.header}
                 </h5>
                 <small>{props.right}</small>
             </div>
-            <p className="mb-1">
-                {props.content}    
-            </p>
+            { props.content ?
+                <p className="mb-1">
+                    {props.content}    
+                </p>
+                : null
+            }
+           
         </div>
     )
 }
