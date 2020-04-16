237ea395524241617e4fdc5f32cf83d78af2f969
sarassoroberto <sarassoroberto@gmail.com>
2020-04-16 06:06:58 +0200

    media

---
+++ src/MediaPlayer/MediaPlayer.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class MediaPlayer extends Component {
+
+    
+
+    render() {
+        return (<div className="media-player">
+                    {this.props}
+                </div>)
+    }
+}
+
+export default MediaPlayer
