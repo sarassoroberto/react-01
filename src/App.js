4bc3405730953575fc05d1a2f381e3a0d43d6c3d
sarassoroberto <sarassoroberto@gmail.com>
2020-04-16 06:51:34 +0200

    media player statico

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 // react.js --> node_module/react/react.js
 // apri il file react.js cerca export default  lo mette dentro la variabile React
-import React from 'react';
+import React, { useState } from 'react';
 import playlist from './playlist.json';
 import ListGroupItem from './ListGroupItem/ListGroupItem';
+import MediaPlayer from './MediaPlayer/MediaPlayer';
 // import './App.css';
 // Componente App  (functional component)
 function App() {
 
+  
+   const [player,setPlayer] =  useState({
+            currentMedia:playlist[0]
+         })
+
   const songlist = playlist.map((song) => {
-    return  (
-             <ListGroupItem 
-                key={song.id}
-                header={song.title}
-                content={song.note}
-                right={song.time}
-             />)
+                  return  (
+                           <ListGroupItem 
+                              key={song.id}
+                              header={song.title}
+                              content={song.note}
+                              right={song.time}
+                           />)
   })
   
   return (<div className="app container"> 
+            <MediaPlayer 
+               media = {player.currentMedia}
+            />
              {songlist}
           </div>);
 
--- src/MediaPlayer/MediaPlayer.js
+++ src/MediaPlayer/MediaPlayer.js
@@ -2,13 +2,16 @@ import React,{Component} from 'react'
 
 class MediaPlayer extends Component {
 
-    
-
     render() {
-        return (<div className="media-player">
-                    {this.props}
+        return (<div class="card media-player">
+                {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+                    <div class="card-body">
+                        <h5 class="card-title">{this.props.media.title}</h5>
+                        <p class="card-text">{this.props.media.time}</p>
+                    </div>
                 </div>)
     }
 }
 
 export default MediaPlayer
+
